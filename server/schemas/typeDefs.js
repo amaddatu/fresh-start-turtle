6e30850cddbaacb92cab736fa1c3cf27ab73df49
amaddatu <>
2023-08-24 17:25:18 -0400

    Add upgraded apollo code for context

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,6 +7,12 @@ const typeDefs = `
     name: String!
   }
 
+  type User {
+    _id: ID!
+    name: String
+    email: String
+  }
+
   type Matchup {
     _id: ID!
     tech1: String!
@@ -19,11 +25,19 @@ const typeDefs = `
     test: Message
     tech: [Tech]
     matchups(_id: String): [Matchup]
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Mutation {
     createMatchup(tech1: String!, tech2: String!): Matchup
     createVote(_id: String!, techNum: Int!): Matchup
+    signUp(name: String!, email: String!, password: String!): Auth 
+    login(email: String!, password: String!): Auth
   }
 `;
 
