0021daa531db886ff974f6745bdba3748c6c7d3c
amaddatu <>
2023-08-28 16:28:08 -0400

    Minor fix on alternate code for redirects

--- client/src/pages/Logout.jsx
+++ client/src/pages/Logout.jsx
@@ -1,9 +1,10 @@
 import Auth from '../utils/auth';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useLogin } from '../utils/LoginContext';
 import { LOGOUT } from '../utils/actions';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Navigate } from 'react-router-dom';
 export default function Logout(){
+  const [redirectState, setRedirectState] = useState(false);
   const [state, dispatch] = useLogin();
   const navigate = useNavigate();
   useEffect( () => {
@@ -13,11 +14,22 @@ export default function Logout(){
     dispatch({
       type: LOGOUT
     });
+    
+    // if you are using the imperative navigate hook, then you do not need redirectState or the Navigate component
     // go back to the homepage
     navigate("/");
+
+    // // alternate version, use the redirect component below and comment out navigate("/");
+    // setRedirectState(true);
   }, []);
   return (
     <>
+      {/* if you are using the imperative navigate hook, then you do not need redirectState or the Navigate component */}
+      {redirectState ? (
+        <Navigate to="/"/>
+      ) : (
+        <></>
+      )}
       Logout
     </>
   );
